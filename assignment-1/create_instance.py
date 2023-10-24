b194141b26048ea4e3252057374e3abb084de1f5
AndrewK0val <andrewkoval2003@gmail.com>
2023-10-24 16:08:23 +0100

    changed some code and handed up assignment 1

--- assignment-1/create_instance.py
+++ assignment-1/create_instance.py
@@ -5,84 +5,219 @@ import time
 import datetime
 import random
 import string
+import subprocess
+import json
+ 
+
+OBJECT_NAME = '/home/andrew/Devops/script.js'
+AWS_REGION='us-east-1'
+OBJECT_1 = 'index.html'
+OBJECT_2 = 'logo.jpg'
+AWS_REGION='us-east-1'
+keyName='CA-1-key'
+instances = []
+
+def random_chars(lenght):
+    RANDOM_CHARS = ''.join(random.choices(string.ascii_lowercase + string.digits, k=lenght))
+    return RANDOM_CHARS
+
+BUCKET_NAME=f'{random_chars(6)}-akoval'
 
 
 private_key_path = 'your-private-key.pem'
-s3 = boto3.resource("s3")
-ec2 = boto3.client('ec2')
+s3_client = boto3.client('s3')
+s3_resource = boto3.resource("s3")
+ec2_client = boto3.client('ec2', region_name='us-east-1')
+ec2 = boto3.resource('ec2', region_name='us-east-1')
 
 user_data_script = """#!/bin/bash
-yum install httpd -y
-systemctl enable httpd
-systemctl start httpd
+    yum update -y
+    yum install httpd -y
+    yum install -y mariadb-server
+    yum install php -y
 
-INSTANCE_TYPE="t2-nano"
-AVILABILITY_ZONE="us-east-1"
-ADDITIONAL_TEXT="abcdefg"
+    systemctl enable httpd
+    systemctl start httpd
+
+    INSTANCE_TYPE="t2-nano"
+    AVILABILITY_ZONE="us-east-1"
+    ADDITIONAL_TEXT="abcdefg"
+
+
+
+
+    cd /var/www/html
+    echo '<html>' > index.html
+    echo '<a href="index.html">Home</a> | <a href="monitoring.html">Monitoring Stats</a><br><br>' >> index.html
+    echo '<ul>' >> index.html
+    echo '<li>Private IP address: ' >> index.html
+    curl http://169.254.169.254/latest/meta-data/local-ipv4 >> index.html
+    echo " Public IP address" >> index.html
+    curl http://169.254.169.254/latest/meta-data/public-ipv4 >> index.html 
+    echo '</li>' >> index.html
+    echo '<li>Instance AMI: ' >> index.html
+    curl -s http://169.254.169.254/latest/meta-data/ami-id >> index.html
+    echo '</li>' >> index.html
+    echo '<li>Availability Zone: ' >> index.html
+    curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone >> index.html
+    echo '</li>' >> index.html
+    echo '<li>Instance ID: ' >> index.html
+    curl -s http://169.254.169.254/latest/meta-data/instance-id >> index.html
+    echo '</li>' >> index.html
+    echo '<li>Instance Type: ' >> index.html
+    curl -s http://169.254.169.254/latest/meta-data/instance-type >> index.html
+    echo '</li>' >> index.html
+    echo '</ul>' >> index.html
+    cp index.html /var/www/html/index.html
+
 """
 
+
+def userMenu():
+    option = input("Please select an option: \n1. Create instance \n2. Create s3 bucket \n3. View bucket details \n4. CloudWatch \n 5. Exit \n")
+
+    match option:
+        case "1":
+            create_instance()
+        case "2":
+            create_s3_bucket(OBJECT_1, OBJECT_2, BUCKET_NAME)
+        case "3":
+            view_bucket_details()
+        case "4":
+            cloudWatch()
+
+        case "5":
+            exit()
+
+
+
+def download_image():
+    URL = "http://devops.witdemo.net/logo.jpg"
+    response = requests.get(URL)
+
+    file = open("logo.jpg", "wb")
+    file.write(response.content)
+    file.close()
+
+
 def create_instance():
-    instance_params = {
-        'ImageId': 'ami-00c6177f250e07ec1', 
-        'MinCount': 1,
-        'MaxCount': 1,
-        'InstanceType': 't2.nano',      
-        'KeyName': 'DevopsAWSKeys', 
-        'UserData': user_data_script,
-        'SecurityGroups': ['launch-wizard-1'] 
-    }
-    # Launch the EC2 instance
-    response = ec2.run_instances(**instance_params)
+    try:
+        print('creating instance...')
+        instance = ec2.create_instances(
+            ImageId='ami-00c6177f250e07ec1',
+            InstanceType='t2.nano',
+            MinCount=1,
+            MaxCount=1,
+            KeyName=keyName,
+            SecurityGroups=['launch-wizard-1'],
+            SecurityGroupIds=['sg-03328d5226bccd389'],
+            UserData=user_data_script
+        )
 
-    # Extract the instance ID
-    instance_id = response['Instances'][0]['InstanceId']
-    # ipAddr = ec2.instances.all().Private_IP_Address
+        instance_id = instance[0].id
+        ec2.create_tags(Resources=[instance_id], Tags=[{'Key': 'Web server', 'Value': 'AssignmentInstance'}]) 
+    
+        instance[0].wait_until_running() #Wait for the instance to be up and running
+        instance[0].reload() #Reload the instance to ensure it reflects the current state
+        # Launch the EC2 instance
+        # response = ec2.run_instances(**instance_params)
 
-    print(f"Launched EC2 instance with ID: {instance_id}")
-    # Wait for the instance to be in a running state
-    ec2.get_waiter('instance_running').wait(InstanceIds=[instance_id])
-    # Describe the instance to get its public IP address
-    response = ec2.describe_instances(InstanceIds=[instance_id])
-    public_ip = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
-    print(f"Public IP address of the instance: {public_ip}")
+        # Extract the instance ID
+        # ipAddr = ec2.instances.all().Private_IP_Address
+
+        print(f"Launched EC2 instance with ID: {instance_id}")
+        # Wait for the instance to be in a running state
+        ec2_client.get_waiter('instance_running').wait(InstanceIds=[instance_id])
+        # Describe the instance to get its public IP address
+
+        subprocess.run("chmod 400 " + str(keyName) + ".pem", shell=True)
+        subprocess.run("scp -i " + str(keyName) + ".pem" + " -o StrictHostKeyChecking=no monitoring.sh ec2-user@" +
+        str(instance[0].public_ip_address) + ":." , shell=True)
+        print("Waiting for instance initialization")
+        time.sleep(60)
+
+        print("checking scp connection")
+        subprocess.run("ssh -i "+ str(keyName) + ".pem" + " ec2-user@" + str(instance[0].public_ip_address) + " 'chmod 700 monitoring.sh'", shell = True)
+        print("checking ssh connection")
+        subprocess.run("ssh -i " + str(keyName) + ".pem" + " ec2-user@" + str(instance[0].public_ip_address) + " ' ./monitoring.sh'", shell = True)
+        response = ec2_client.describe_instances(InstanceIds=[instance_id])
+        public_ip = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
+        print(f"Public IP address of the instance: {public_ip}")
+
+    except Exception as e:
+        print(f"An error occurred while creating the EC2 instance: {e}")
+
 
 
 # s3 bucket code
 #---------------------------------------------------------
 
-#You should name your bucket using a combination of 6 random characters and your
-#name; e.g. 1a2b3c-jbloggs replacing 1a2b3c with random characters and jbloggs with your first
-#initial and last name. 
-lenght = 6
-sixRandomChars = ''.join(random.choices(string.ascii_letters + string.digits, k=lenght))
+# sixRandomChars = ''.join(random.choices(string.ascii_letters + string.digits, k=lenght))
 
 
 
-bucket_name = sixRandomChars + "-akoval"
 
-try:
-    response = s3.create_bucket(Bucket=bucket_name)
-    print(response)
-except Exception as error:
-    print(error)
+def create_s3_bucket(OBJECT_1, OBJECT_2, BUCKET_NAME):
+    
+    website_configuration = { 
+            'ErrorDocument': {'Key': 'error.html'}, 
+            'IndexDocument': {'Suffix': 'index.html'}, 
+        } 
+    s3_bucket = boto3.resource("s3") 
+    if __name__ == '__main__':
+        print("making bucket...")
+    try:
+        s3_resource.create_bucket(Bucket=BUCKET_NAME)
+        print(f"AWS s3 bucket '{BUCKET_NAME}' has been created")
+        s3_client.delete_public_access_block(Bucket=BUCKET_NAME)
+        bucket_policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "PublicReadGetObject",
+                    "Effect": "Allow",
+                    "Principal": "*",
+                    "Action": ["s3:GetObject"],
+                    "Resource": f"arn:aws:s3:::{BUCKET_NAME}/*"
+                }
+            ]
+            }
+        s3_resource.Bucket(BUCKET_NAME).Policy().put(Policy=json.dumps(bucket_policy))
+        bucket_website = s3_resource.BucketWebsite(BUCKET_NAME)
+        response = bucket_website.put(WebsiteConfiguration=website_configuration)
+        response = s3_bucket.Object(BUCKET_NAME, OBJECT_1).put(Body=open(OBJECT_1, 'rb'), ContentType = 'text/html')
+        response = s3_bucket.Object(BUCKET_NAME, OBJECT_2).put(Body=open(OBJECT_2, 'rb'), ContentType = 'image/jpeg')
+
+        print(f"AWS s3 bucket '{BUCKET_NAME}' has been created")
+        print(f"bucket website:http://{BUCKET_NAME}.s3-website.{AWS_REGION}.amazonaws.com")
+        print(response)
+        webbrowser.open_new_tab(f'http://{BUCKET_NAME}.s3-website.{AWS_REGION}.amazonaws.com')
+    except Exception as error:
+        print(error)
+
+def view_bucket_details():
+    try:
+        for bucket in s3_resource.buckets.all():
+            print(bucket.id, bucket.state, bucket.public_ip_address)
+    except Exception as e:
+        print(e)
+
+# create_instance()
+# create_s3_bucket(OBJECT_1, OBJECT_2, BUCKET_NAME)
+
+
+# print("Opening the web browser...")
+# time.sleep(30)
 
 
 
-website_configuration = { 
-    'ErrorDocument': {'Key': 'error.html'}, 
-    'IndexDocument': {'Suffix': 'index.html'}, 
-} 
- 
-bucket_website = s3.BucketWebsite({bucket_name})   # replace with your  bucket name or a string variable
- 
- 
-response = bucket_website.put(WebsiteConfiguration=website_configuration)
+def cloudWatch():
+    try: 
+        instance_id = instances[0].id
+    except Exception as e:
+        print(e)
 
-
-print("Opening the web browser...")
-time.sleep(60)
-
-
-
-webbrowser.open_new_tab(f'http://{public_ip}')
-
+def exit():
+    sys.exit()
+        
+userMenu()
--- assignment-1/create_s3_bucket.py
+++ assignment-1/create_s3_bucket.py
@@ -1,38 +1,83 @@
 import boto3
 import string
 import random
+import json
+import webbrowser
 
-lenght = 6
-sixRandomChars = ''.join(random.choices(string.ascii_lowercase + string.digits, k=lenght))
-OBJECT_NAME = '/home/andrew/Devops/script.js'
+s3_resource = boto3.resource("s3")
+s3_client = boto3.client('s3')
 
-BUCKET_NAME=f'{sixRandomChars}-akoval'
+
+def random_chars(lenght):
+    RANDOM_CHARS = ''.join(random.choices(string.ascii_lowercase + string.digits, k=lenght))
+    return RANDOM_CHARS
+
+OBJECT_1 = 'index.html'
+OBJECT_2 = 'logo.jpg'
+
+BUCKET_NAME=f'{random_chars(6)}-akoval'
 AWS_REGION='us-east-1'
 
+# def create_s3_bucket_using_client(bucket_name, aws_region):
+#     # s3_bucket_location = {'LocationConstraint': aws_region}
+#     response  = s3_resource.create_bucket(Bucket=bucket_name)
+#     s3_client.delete_public_access_block(Bucket=bucket_name)
 
-def create_s3_bucket_using_client(bucket_name, aws_region):
-    s3_client = boto3.client('s3')
-    # s3_bucket_location = {'LocationConstraint': aws_region}
+#     bucket_policy = {
+#         "Version": "2012-10-17",
+#         "Statement": [
+#             {
+#                 "Sid": "PublicReadGetObject",
+#                 "Effect": "Allow",
+#                 "Principal": "*",
+#                 "Action": ["s3:GetObject"],
+#                 "Resource": f"arn:aws:s3:::{bucket_name}/*"
+#             }]
+#     }
 
-    response  = s3_client.create_bucket(
-        Bucket=bucket_name
-        # CreateBucketConfiguration=s3_bucket_location
-    )
+#     s3_resource.Bucket(bucket_name).Policy().put(Policy=json.dumps(bucket_policy))
+#     return response
 
-    return response
-
-
-def put_stuff_in_bucket(OBJECT_NAME, BUCKET_NAME):
+def create_s3_bucket(OBJECT_1, OBJECT_2, BUCKET_NAME):
+    
+    website_configuration = { 
+            'ErrorDocument': {'Key': 'error.html'}, 
+            'IndexDocument': {'Suffix': 'index.html'}, 
+        } 
     s3_bucket = boto3.resource("s3") 
-
-
     if __name__ == '__main__':
-        create_s3_bucket_using_client(BUCKET_NAME, AWS_REGION)
-        print(f"AWS s3 bucket '{BUCKET_NAME}' has been created")
-
+        print("making bucket...")
     try:
-        response = s3_bucket.Object(BUCKET_NAME, OBJECT_NAME).put(
-            Body=open(OBJECT_NAME, 'rb'))
+        # create_s3_bucket_using_client(BUCKET_NAME, AWS_REGION)
+
+        s3_resource.create_bucket(Bucket=BUCKET_NAME)
+        print(f"AWS s3 bucket '{BUCKET_NAME}' has been created")
+        s3_client.delete_public_access_block(Bucket=BUCKET_NAME)
+        bucket_policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+            {
+                "Sid": "PublicReadGetObject",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": ["s3:GetObject"],
+                "Resource": f"arn:aws:s3:::{BUCKET_NAME}/*"
+                }
+                ]
+            }
+        s3_resource.Bucket(BUCKET_NAME).Policy().put(Policy=json.dumps(bucket_policy))
+        bucket_website = s3_resource.BucketWebsite(BUCKET_NAME)
+        response = bucket_website.put(WebsiteConfiguration=website_configuration)
+        response = s3_bucket.Object(BUCKET_NAME, OBJECT_1).put(Body=open(OBJECT_1, 'rb'), ContentType = 'text/html')
+        response = s3_bucket.Object(BUCKET_NAME, OBJECT_2).put(Body=open(OBJECT_2, 'rb'), ContentType = 'image/jpeg')
+
+        print(f"AWS s3 bucket '{BUCKET_NAME}' has been created")
+        print(f"bucket website:http://{BUCKET_NAME}.s3-website.{AWS_REGION}.amazonaws.com")
         print(response)
     except Exception as error:
         print(error)
+
+create_s3_bucket(OBJECT_1, OBJECT_2, BUCKET_NAME)
+webbrowser.open_new_tab(f'http://{BUCKET_NAME}.s3-website.{AWS_REGION}.amazonaws.com')
+
+
